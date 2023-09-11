f812195d79f5703b7a0193c97d136f116ba42b01
harrysdc <39647707+harrysdc@users.noreply.github.com>
2023-09-11 19:18:17 -0400

    add effective sample size

--- filter/PF.py
+++ filter/PF.py
@@ -15,18 +15,18 @@ rng = default_rng()
 
 class PF:
     # PF construct an instance of this class
-    #
     # Inputs:
     #   system: system and noise models
     #   init:   initial state mean and covariance
 
     def __init__(self, system, init):
+        # PF needs no Jacobian
         np.random.seed(2)
         self.gfun = system.gfun  # motion model
         self.hfun = system.hfun  # measurement model
         self.M = system.M # covariance of motion noise
         self.Q = system.Q # covariance of measurement noise
-        
+
         # PF parameters
         self.n = init.n
         self.Sigma = init.Sigma # (3, 3) diagnoal matrix
@@ -42,58 +42,45 @@ class PF:
         # TODO: Implement the prediction step for PF                                  #
         # Hint: Propagate your particles. Particles are saved in self.particles       #
         # Hint: Use rng.standard_normal instead of np.random.randn.                   #
-        #       It is statistically more random.#                                     #
+        #       It is statistically more random.                                      #
         ###############################################################################
-        '''
-        u is control input of shape (3,)
-        PF needs no Jacobian
-        '''
         
-        for i in range(self.n):
-            noise = self.M(u).diagonal() * rng.standard_normal(3)
-            u += noise
-            self.particles[:, i] = self.gfun(self.particles[:, i], u)
+        u_noise_std = np.linalg.cholesky(self.M(u))
+        for j in range(self.n):
+            sample_action = u.reshape(-1,1) + u_noise_std @ rng.standard_normal((3,1))
+            self.particles[:,j] = self.gfun(self.particles[:,j], sample_action.reshape(3))
 
     def correction(self, z, landmarks):
         ###############################################################################
         # TODO: Implement the correction step for PF                                  #
-        # Hint: self.mean_variance() will update the mean and covariance              #
         # Hint: you can use landmark1.getPosition()[0] to get the x position of 1st   #
         #       landmark, and landmark1.getPosition()[1] to get its y position        #
+        # Hint: landmarks -- (6, 2) landmarks 2d positions in world coordniate        #
+        #       z         -- (6, ) each half is [bearing, range, landmark_id]         #
         ###############################################################################
-        '''
-        landmarks -- (6, 2) landmarks 2d positions in world coordniate
-        z -- (6, ) each half is [bearing, range, landmark_id]
-        '''
-        
         landmark1 = landmarks.getLandmark(z[2].astype(int))
         landmark2 = landmarks.getLandmark(z[5].astype(int))
         
-        def likelihood(z, x, landmark1, landmark2):
-            '''
-            z -- (6, ) each half is [bearing, range, landmark_id]
-            x -- robot state (3, )
-            '''
-            # compute expected bearing and range to landmark1 and landmark2 for a particle
-            # compute difference in expected measurement and real measurement
-            z1_diff = z[0:2] - self.hfun(landmark1.getPosition()[0], landmark1.getPosition()[1], x)
-            z2_diff = z[3:5] - self.hfun(landmark2.getPosition()[0], landmark2.getPosition()[1], x)
-            z1_diff[1] = wrap2Pi(z1_diff[1])
-            z2_diff[1] = wrap2Pi(z2_diff[1])
-
-            # compute the likelihood of the measurements for each particle
-            likelihoods1 = multivariate_normal.pdf(z1_diff, mean=np.zeros(2), cov=self.Q)
-            likelihoods2 = multivariate_normal.pdf(z2_diff, mean=np.zeros(2), cov=self.Q)
-            likelihoods = likelihoods1 * likelihoods2
-            return likelihoods
-
+        weight = np.zeros(self.n)
         for i in range(self.n):
-            self.particle_weight[i] = likelihood(z, self.particles[:, i], landmark1, landmark2)
+            z_hat1 = self.hfun(landmark1.getPosition()[0], landmark1.getPosition()[1], self.particles[:,i])
+            z_hat2 = self.hfun(landmark2.getPosition()[0], landmark2.getPosition()[1], self.particles[:,i])
+            diff = np.array([
+                wrap2Pi(z[0] - z_hat1[0]),
+                z[1] - z_hat1[1],
+                wrap2Pi(z[3] - z_hat2[0]),
+                z[4] - z_hat2[1]])
+            weight[i] = multivariate_normal.pdf(diff, np.zeros(4), cov=block_diag(self.Q, self.Q))
 
-        # normalize weights
+        self.particle_weight *= weight
         self.particle_weight /= np.sum(self.particle_weight)
+        
+        # effective sample size
+        Neff = 1 / np.sum(self.particle_weight ** 2)
+        if Neff < self.n / 5:
+            self.resample()
 
-        # compute posterior pose
+        # compute the final pose
         self.mean_variance()
 
     def resample(self):
@@ -130,8 +117,8 @@ class PF:
         sinSum = 0
         cosSum = 0
         for s in range(self.n):
-            cosSum += np.cos(self.particles[2,s])
-            sinSum += np.sin(self.particles[2,s])
+            cosSum += np.cos(self.particles[2,s]) * self.particle_weight[s]
+            sinSum += np.sin(self.particles[2,s]) * self.particle_weight[s]
         X[2] = np.arctan2(sinSum, cosSum)
 
         zero_mean = np.zeros_like(self.particles)
